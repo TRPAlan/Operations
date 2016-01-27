ed2d85b36ff8b2f40b656730e366a8c7d998b001
lucy-theRedPin <lucyyu@Lucys-MacBook-Pro.local>
2016-01-27 16:56:04 -0500

    set port

--- service.js
+++ service.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
 console.log('LUCY IN APPLICATION');
 
-app.get('/app', function (req, res) {
+app.get('/', function (req, res) {
   console.log('LUCY IN GET');
   res.send('Hello World!');
   //if (req.param.hub.challenge) res.send(req.param.hub.challenge);
 });
 
-app.post('/app', function (req, res) {
+app.post('/', function (req, res) {
   res.send('Got a POST request');
 });
