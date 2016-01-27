9989d173426fa404d927f7bd57a45e6fb64239d0
lucy-theRedPin <lucyyu@Lucys-MacBook-Pro.local>
2016-01-27 16:33:27 -0500

    init

--- service.js
+++ service.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.configure(function(){
-    app.use(express.bodyParser());    
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+  if (req.param.hub.challenge) res.send(req.param.hub.challenge);
 });
 
-
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
