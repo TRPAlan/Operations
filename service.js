37f97340f9c15d399277b0bdee20875c509f0ab8
lucy-theRedPin <lucyyu@Lucys-MacBook-Pro.local>
2016-01-28 13:50:10 -0500

    123

--- service.js
+++ service.js
@@ -9,20 +9,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/facebook', express.static(__dirname + '/views'));
 
+// for authentication, our token is theredpin (go to facebook developer > webhooks )
 app.get('/FacebookLeadGen', function (req, res) {
-
-	// get verify token from facebook and send response accordingly
+  // get verify token from facebook and send response accordingly
   if (req.param('hub.verify_token') == 'theredpin') {
   	res.send(req.param('hub.challenge'));
   } else {
 	res.send('Hello World!');
   }
-
 });
 
 app.post('/FacebookLeadGen', function (req, res) {
   console.log('LUCY DEBUG: Got a POST request');
   console.log(req.body); 
+
+  var object = req.object; 
+  var changes[] = req.body.entry.changes; 
+  console.log ('object:' + object);
+  
+  for (change in changes) {
+  	console.log('change field:' + change.field); 
+  }	
+
   res.send('yay');
 }); 
 
