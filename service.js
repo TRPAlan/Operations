f7c1a1031c4550fe003f9294035e93223ce9fe43
lucy-theRedPin <lucyyu@Lucys-MacBook-Pro.local>
2016-01-28 11:11:53 -0500

    subscribe page

--- service.js
+++ service.js
@@ -3,6 +3,8 @@ var app = express();
 
 //console.log('LUCY IN APPLICATION');
 
+app.use('/facebook', express.static(__dirname + '/views'));
+
 app.get('/FacebookLeadGen', function (req, res) {
 
 	// get verify token from facebook and send response accordingly
@@ -17,6 +19,11 @@ app.get('/FacebookLeadGen', function (req, res) {
 app.post('/FacebookLeadGen', function (req, res) {
   res.send('Got a POST request');
 }); 
+
+/*
+app.get('/SubscribeFacebookPage', function(req, res) {
+	res.render('/index'); 
+});*/
 
 app.listen( process.env.PORT || 5000, function() {
   console.log('Node app is running on port', process.env.PORT);
