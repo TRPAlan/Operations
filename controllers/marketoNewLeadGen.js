abe1aafce8be490fdf6a4fbdfc218f21621917ce
lucyyu24 <lucyyu1996@hotmail.com>
2016-02-03 11:13:27 -0500

    async waterfall

--- controllers/marketoNewLeadGen.js
+++ controllers/marketoNewLeadGen.js
@@ -1,5 +1,6 @@
 var https = require('https');
 var unirest = require('unirest');
+var async = require('async');
 
 var marketoCallback = function (response, formId, name, email, phone) {
   var str = '';
@@ -58,7 +59,7 @@ var insertLeadCallback = function(response, formId) {
         console.log('lead phone: ' + dataList[i].values[0]);
         phone = dataList[i].values[0];
       }
-  }
+    }
 
   var callback = function (response) {
     marketoCallback(response, formId, name, phone, email);
@@ -97,38 +98,57 @@ exports.post = function (req, res) {
     // log out information related to lead
       console.log('change.leadgenID: ' + req.body.entry[0].changes[i].value.leadgen_id);
       console.log('change.formID: ' + req.body.entry[0].changes[i].value.form_id);
-
       console.log('change.created_time: ' + req.body.entry[0].changes[i].value.created_time);
-
-      var callback = function (response) {
-        insertLeadCallback(response, req.body.entry[0].changes[i].value.form_id);
-      }; 
-
-      https.request({
-          host: 'graph.facebook.com',
-          path: '/' + leadGenId + '?access_token='+ process.env.FACEBOOK_PAGE_TOKEN 
-      }, callback).end();
-
-    }
+      chainedRequests(req.body.entry[0].changes[i].value.leadgen_id, req.body.entry[0].changes[i].value.form_id); 
+  }
 
   res.send('postFacebookLeadGen SUCCESS');
 }
 
-/*
-    var bodyString = JSON.stringify({
-      action: 'createOrUpdate',
-      lookupField: 'email',
-      input: [
-        {
-          email: email,
-          firstName: name,
-          phone: phone,
-        }
-      ]
-  });
-    console.log(bodyString);
 
-    var headers = {
-      'Content-Type': 'application/json',
-      'Content-Length': bodyString.length
-  };*/
+var chainedRequests = function (leadGenId, formId) {
+  async.waterfall([
+    // get lead information using leadGenId
+    function(callback) {
+      unirest.get('https://graph.facebook.com/' + leadGenId + '?access_token='+ process.env.FACEBOOK_PAGE_TOKEN) 
+      .end(function (response) {
+        
+        var dataList = JSON.parse(response.body).field_data;
+        var name, email, phone; 
+
+        for (var i=0; i< dataList.length; i++){
+          if (dataList[i].name == 'full_name') {
+            console.log('lead name: ' + dataList[i].values[0]);
+            name = dataList[i].values[0];
+          }
+          if (dataList[i].name == 'email') {
+            console.log('lead email: ' + dataList[i].values[0]);
+            email = dataList[i].values[0];
+          }
+          if (dataList[i].name == 'phone_number') {
+            console.log('lead phone: ' + dataList[i].values[0]);
+            phone = dataList[i].values[0];
+          }
+        }
+
+        callback(null, name, email, phone);
+
+      });
+    },
+
+    function(name, email, phone, callback) {
+      unrest.get('https://615-KOO-288.mktorest.com/identity/oauth/token?grant_type=client_credentials&client_id=' + process.env.MKT_CLIENT_ID + '&client_secret=' + process.env.MKT_CLIENT_SECRET)
+      .end(function (response) {
+        var firstName = name.split(' ').slice(0, -1).join(' ');
+        var lastName = name.split(' ').slice(-1).join(' ');
+        console.log('mkt access token:' + JSON.parse(response.body).access_token); 
+        console.log ('INSERT LEAD: ' + name + ' ' + email + ' ' + phone + ' ' + formId); 
+
+      });
+
+    }
+
+  ]);
+};
+
+
